e1d8be24b33438029623e6f318e5a79391030148
Dan Kirkwood <dangogh@gmail.com>
2009-06-14 00:04:58 -0600

    attempt to add some regex tests -- not complete

--- t/30-regex.C
+++ t/30-regex.C
@@ -1,10 +1,19 @@
 #include "perl++.h"
 #include "tap++.h"
-#include <climits>
 
 using namespace perl;
 using namespace TAP;
 
 int main() {
-	plan(skip_all, "Tests not implemented yet");
+	plan(2);
+	Interpreter universe;
+
+   Regex word_at_start = universe.regex("^\\w\\+");
+   Regex word_at_end = universe.regex("\\w\\+$");
+   String t1 = universe.value_of("wassup?");
+   ok(t1.match(word_at_start), "Matched start of string");
+
+   // TODO: shouldn't this return a false value?
+   not_ok(t1.match(word_at_end), "Didn't match word at end of string");
+
 }
